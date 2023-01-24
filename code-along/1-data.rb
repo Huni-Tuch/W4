26bd09ea721e6d87be9263a9c0242551463dc7ef
Huni-Tuch <hunibert.tuch@kellogg.northwestern.edu>
2023-01-24 19:58:11 +0000

    dealt with numbers

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -6,9 +6,22 @@
 
 # Numbers
 
+puts 5 # output
+puts 2 # output
+
 # Perform simple math with numbers
 
+puts 2 + 5 # addition
+puts 2 * 5 # multiplication
+puts 2/3 # division
+puts 2 % 5 # remainder
+puts 2.0 / 3.0 # changing to float
+puts 2.0 / 5.0 + 1.0 # example of order of operations
+puts 2.0 / (5.0 + 1.0) # example of order of operations
+
 # Strings
+
+
 
 # Combine strings together
 
